0d809c2268c94c067c8101e8c23bf88d1be82711
pupil <pupil@ivkhk.ee>
2023-09-05 10:43:17 +0300

    first commit

---
+++ src/JPTV22NizamovFirst.java
@@ -0,0 +1,16 @@
+    import java.util.Scanner;
+
+
+public class JPTV22NizamovFirst {
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String firstname = scanner.nextLine();
+        System.out.println("Enter your lastname123123123");
+        String lastname = scanner.nextLine();
+        System.out.println("Hello " + " " + firstname + " " + lastname);
+    }
+    
+}
