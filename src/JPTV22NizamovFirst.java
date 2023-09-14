dc3155f09c6e17111d1222d832504db93d06248a
pupil <pupil@ivkhk.ee>
2023-09-14 15:03:07 +0300

    создание нового функционала программы

--- src/JPTV22NizamovFirst.java
+++ src/JPTV22NizamovFirst.java
@@ -5,15 +5,22 @@ public class JPTV22NizamovFirst {
 
     
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name");
-        String firstname = scanner.nextLine();
-        System.out.println("Enter your lastname123123123");
-        String lastname = scanner.nextLine();
-        System.out.println("Hello " + " " + firstname + " " + lastname);
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter your name");
+//        String firstname = scanner.nextLine();
+//        System.out.println("Enter your lastname123123123");
+//        String lastname = scanner.nextLine();
+//        System.out.println("Hello " + " " + firstname + " " + lastname);
+//        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//        
+        
+        app app = new app();
+        //app.setActive(true);
+        //System.out.println("app.toString = " + app.toString());
+        app.run();
+    
     }
     
 }
